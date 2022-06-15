5d518b4a6629a5ce57d6c42f79c8913d43679ff0
Kun Kaewken <kunkk01@gmail.com>
2022-06-15 11:30:21 -0500

    Create Item Model With EntityFramework Support

---
+++ WishList/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WishList.Models;
+
+namespace WishList.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Item> Items;
+        ApplicationDbContext(DbContextOptions options) : base(options);
+
+    }
+}
